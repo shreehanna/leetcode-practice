7be147d9b2730010622ac901df631d5ba5c164f3
Shreehanna <shreehannamathew24@gmail.com>
2025-07-07 01:58:50 +0530

    feat: add Valid Anagram + fix hashmap logic in Two Sum

--- leetcode-practice/easy/001_two_sum.py
+++ leetcode-practice/easy/001_two_sum.py
@@ -1,6 +1,13 @@
 class Solution:
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        seen={}
+        for i , j in enumerate(nums):
+            word=target - j
+            if word in seen:
+                return [seen[word], i]
+            seen[j] = i
+if __name__ == '__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+    s = Solution()
+    print(s.twoSum(nums, target))
